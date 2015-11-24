cf1ad1433bb1732c52d0eac135739887e5b160ce
Glenn Lewis <gmlewis@google.com>
2015-11-24 15:55:37 -0800

    go-frp: update package comments
    Change-Id: I5f8958e8208143db84b62106b6914ca3501bd53b

--- examples/reverse/reverse/reverse.go
+++ examples/reverse/reverse/reverse.go
@@ -1,3 +1,6 @@
+// Package reverse is an example using go-frp modeled after the example found in
+// the book "Functional Reactive Programming" by Stephen Blackheath and Anthony Jones:
+// http://www.manning.com/books/functional-reactive-programming
 package reverse
 
 import h "github.com/gmlewis/go-frp/html"
